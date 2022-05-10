6be10be84010df6c1a16e1c5d60878245fe7af3b
wengxiangmin <157215725@qq.com>
2022-05-10 21:47:44 +0800

    refactor: consistent-indexed-object-style

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -23,7 +23,7 @@ module.exports = {
     '@typescript-eslint/type-annotation-spacing': ['error', {}],
     '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-    '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+    '@typescript-eslint/consistent-indexed-object-style': 'off',
     '@typescript-eslint/prefer-ts-expect-error': 'error',
 
     // Override JS
